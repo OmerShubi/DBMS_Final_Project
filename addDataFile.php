5de4089bd2c13bbd115753a52c1eab41bf30c1fb
OS\Shubi <omer.shubi@gmail.com>
2019-05-07 21:32:58 +0300

    style

--- addDataFile.php
+++ addDataFile.php
@@ -1,1 +1,10 @@
-<?php
+<html>
+<body>
+    <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST" enctype="multipart/form-data">
+
+        <input name="csv" type="file" id="csv" />
+        <input type="submit" name="submit" value="submit" />
+
+    </form>
+</body>
+</html>
