b8dd0bfec870ba6804ba7bb0e4d7009a0526597b
OS\Shubi <omer.shubi@gmail.com>
2019-05-07 20:57:21 +0300

    update text

--- index.php
+++ index.php
@@ -11,6 +11,19 @@
         Premier League Pro Analytics is an advanced Database & Analytics tool <br>
         <i>for Professionals by Professionals (not newbies)!</i>
     </h2>
+    <h3>
+        <a href="addDataFile.php" target="mainFrame">ADD DATA FILE</a><br>
+        <a href="insertData.php" target="mainFrame">INSERT DATA</a><br>
+
+
+    </h3>
+
+
+
+
+
+
+
 
     <?php
     include 'db_connection.php';
