97740d5e83958e82f3f014bbafab6d09bf3040a6
OS\Shubi <omer.shubi@gmail.com>
2019-05-08 01:15:55 +0300

    insertion form

--- index.php
+++ index.php
@@ -17,15 +17,14 @@
             <i>for Professionals by Professionals (not newbies)!</i><br><br>
         </h2>
         <h3>
-            <a href="displayTable.php" target="displayTable">ADD DATA FILE</a>
+            <a href="displayTable.php" target="MainFrame">DISPLAY TABLE</a>
             &nbsp;&nbsp;&nbsp;
-            <a href="addDataFile.php" target="addDataFile">ADD DATA FILE</a>
+            <a href="addDataFile.php" target="MainFrame">ADD DATA FILE</a>
             &nbsp;&nbsp;&nbsp;
-            <a href="insertData.php" target="insertData">INSERT DATA</a><br>
+            <a href="insertData.php" target="MainFrame">INSERT DATA</a><br>
         </h3>
-        <iframe name="addDataFile" width="79%" height="100%" src="addDataFile.php"></iframe>
-        <iframe name="insertData" width="79%" height="100%" src="insertData.php"></iframe>
-        <iframe name="displayTable" width="79%" height="100%" src="displayTable.php"></iframe>
+        <iframe name="MainFrame" width="79%" height="100%" src="index.php"></iframe>
+
     </div>
 </body>
 </html>
