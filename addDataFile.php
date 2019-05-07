0e3406d47defa99334818bd95d5eb1f9f953be21
OS\Shubi <omer.shubi@gmail.com>
2019-05-07 21:42:22 +0300

    data insertion

--- addDataFile.php
+++ addDataFile.php
@@ -6,5 +6,21 @@
         <input type="submit" name="submit" value="submit" />
 
     </form>
+
+    <?php
+        if (isset($_POST["submit"])){
+        // Connect to the database
+            $file = $_FILES[csv][tmp_name];
+            if (($handle = fopen($file, "r")) !== FALSE) {
+                while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                    $sql="INSERT INTO PremierLeague (id, Home, Away, notes, home_goals, away_goals, result, season) VALUES 
+                    ('".addslashes($data[0])."','".addslashes($data[1])."','"
+                        .addslashes($data[2])."','".addslashes($data[3])."','"
+                        .addslashes($data[4])."','".addslashes($data[5])."','"
+                        .addslashes($data[6])."','".addslashes($data[7])."'); ";
+                 sqlsrv_query($conn, $sql);
+                    }
+                  fclose($handle); } }
+    ?>
 </body>
 </html>
