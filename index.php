5734e4ce70de730645240f8d0a3e87ce5d1ace20
OS\Shubi <omer.shubi@gmail.com>
2019-05-08 03:36:22 +0300

    iframe styling

--- index.php
+++ index.php
@@ -3,28 +3,28 @@
 <head>
     <link rel="stylesheet" href="style.css" type="text/css">
     <title>Premier Pro Analytics</title>
-    <link href="favicon.png" rel="icon" type="image/x-icon" />
+    <link href="favicon.png" rel="icon" type="image/x-icon"/>
 </head>
 
 <body class="main">
 
-        <img src="banner.jpg" alt="Premier League Logo" style="width: 80%; border: 1px solid black">
+<img src="banner.jpg" alt="Premier League Logo" style="width: 80%; border: 1px solid black">
 
-    <div class="layer" style="text-align: center">
+<div class="layer" style="text-align: center">
 
     <h2>
-            <br>Premier League Pro Analytics is an advanced Database & Analytics tool <br><br>
-            <i>for Professionals by Professionals (not newbies)!</i><br><br>
-        </h2>
-        <h3>
-            <a href="displayTable.php" target="MainFrame">DISPLAY TABLE</a>
-            &nbsp;&nbsp;&nbsp;
-            <a href="addDataFile.php" target="MainFrame">ADD DATA FILE</a>
-            &nbsp;&nbsp;&nbsp;
-            <a href="insertData.php" target="MainFrame">INSERT DATA</a><br>
-        </h3>
-        <iframe name="MainFrame"></iframe>
+        <br>Premier League Pro Analytics is an advanced Database & Analytics tool <br><br>
+        <i>for Professionals by Professionals (not newbies)!</i><br><br>
+    </h2>
+    <h3>
+        <a href="displayTable.php" target="MainFrame">DISPLAY TABLE</a>
+        &nbsp;&nbsp;&nbsp;
+        <a href="addDataFile.php" target="MainFrame">ADD DATA FILE</a>
+        &nbsp;&nbsp;&nbsp;
+        <a href="insertData.php" target="MainFrame">INSERT DATA</a><br>
+    </h3>
+    <iframe name="MainFrame"></iframe>
 
-    </div>
+</div>
 </body>
 </html>
