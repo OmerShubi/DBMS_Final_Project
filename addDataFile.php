3002e96511869401410b536bc05ec26349716e6f
shubi <omer.shubi@gmail.com>
2019-05-08 12:22:41 +0300

    General styling

--- addDataFile.php
+++ addDataFile.php
@@ -4,11 +4,16 @@
     <title>Premier Pro Analytics | File</title>
 </head>
 <body style="text-align:center">
-<h2 style="font-size:20px;color:white">Whole new Database? Upload CSV File here!</h2>
-<?php
-    include 'db_connection.php';
-    $conn = OpenCon();
+    <h2 style="font-size:20px;color:white">
+        Whole new Database? Upload CSV File here!
+    </h2>
+
+    <?php
+        include 'db_connection.php';
+        $conn = OpenCon();        // Connect to the database
     ?>
+
+<!--    File Selection   -->
     <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST" enctype="multipart/form-data">
 
         <input name="csv" type="file" id="csv" />
@@ -16,17 +21,22 @@
 
     </form>
 
+<!--    Insert data into database   -->
     <?php
-
-    if (isset($_POST["submit"])){
-        // Connect to the database
+        if (isset($_POST["submit"]))
+        {
             $file = $_FILES[csv][tmp_name];
-            if (($handle = fopen($file, "r")) !== FALSE) {
+            if (($handle = fopen($file, "r")) !== FALSE)
+            {
                 // Assuming first row stores column name --> skip insertion
                 $data = fgetcsv($handle, 1000, ",");
-                $counter = 1;
-                while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-                    $sql="INSERT INTO PremierLeague (id, Home, Away, home_goals, away_goals, result, season, notes) VALUES 
+
+                // Insert data into database
+                $counter = 1; // Create auto incrementing id
+                while (($data = fgetcsv($handle, 1000, ",")) !== FALSE)
+                {
+                    $sql="INSERT INTO PremierLeague
+                            (id, Home, Away, home_goals, away_goals, result, season, notes) VALUES 
                     ('".addslashes($counter)."','".addslashes($data[0])."','".addslashes($data[1])."','"
                         .addslashes($data[2])."','".addslashes($data[3])."','".addslashes($data[4])."','"
                         .addslashes($data[5])."','".addslashes($data[6])."'); ";
--- insertData.php
+++ insertData.php
@@ -4,91 +4,101 @@
     <title>Premier Pro Analytics | Insert</title>
 </head>
 <body style="text-align:center">
-<h2 style="font-size:20px;color:white">Another game played? Add it here!</h2>
-<?php
-    include 'db_connection.php';
-    $conn = OpenCon();
-?>
-<form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
-    <table class="insertion_table">
-        <tr>
-            <td><label for="Home">Home Team:</label></td>
-            <td><select name="Home">
-                <option value="">Choose home team</option>
-                <?php
-                    $sql = "SELECT * FROM PremierLeague";
-                    $result = sqlsrv_query($conn, $sql);
-                    while($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC))
-                    {
-                        echo "<option value=".$row['Home'].">".addslashes($row['Home'])."</option>";
-                    }
-                ?>
-            </select></td>
-        </tr>
-        <tr>
-            <td><label for="Away">Away Team:</label></td>
-            <td><select name="Away">
-                <option value="">Choose away team</option>
-                <?php
-                $sql = "SELECT * FROM PremierLeague";
-                $result = sqlsrv_query($conn, $sql);
-                while($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC))
-                {
-                    echo "<option value=".$row['Away'].">".addslashes($row['Away'])."</option>";
-                }
-                ?>
-            </select></td>
-        </tr>
-        <tr>
-            <td><label for="result">Result:</label></td>
-        </tr>
-        <tr>
-            <td><input name="result" type="radio" value="H"><label for="H">H</label><br>
-                <input name="result" type="radio" value="D"><label for="D">D</label><br>
-                <input name="result" type="radio" value="A"><label for="A">A</label></td>
-        </tr>
-        <tr style="padding-bottom: 5px">
-            <td><label for="home_goals">Home Goals</label></td>
-            <td><input name="home_goals" type="range" step="1" min="0" max="10"></td>
-        </tr>
-        <tr style="padding-bottom: 5px">
-            <td><label for="away Goals">Away Goals</label></td>
-            <td><input name="away_goals" type="range" step="1" min="0" max="10"></td>
-        </tr>
-        <tr style="padding-bottom: 5px">
-            <td><label for="season">Season*</label></td>
-            <td><input name="season" type="text" size="24" required></td>
-        </tr>
-        <tr style="padding-bottom: 5px">
-            <td colspan="2"><textarea name="notes" Rows="5" cols="36" placeholder="Write here a special note"></textarea></td>
-        </tr>
-        <tr>
-            <td><input name="submit" type="submit" value="Add Result"></td>
-            <td><input name="reset" type="reset" value="Reset Page"></td>
-        </tr>
-    </table>
-</form>
-    <?php
-    if (isset($_POST["submit"]))
-    {
-        // Get last ID, increment by one and set as new ID
-        $sql = "SELECT max(id) newID FROM PremierLeague";
-        $IDtable = sqlsrv_query($conn, $sql);
-        $row = sqlsrv_fetch_array($IDtable, SQLSRV_FETCH_ASSOC);
-        $id = $row['newID'] + 1;
 
-        // Insert data into PremierLeague table
-        $sql = "INSERT INTO PremierLeague (id, Home, Away, notes, home_goals, away_goals, result, season) VALUES
-    ('".addslashes($id)."','".addslashes($_POST['Home'])."','".addslashes($_POST['Away'])."'
-    ,'".addslashes($_POST['notes'])."','".addslashes($_POST['home_goals'])."','".addslashes($_POST['away_goals'])."'
-    ,'".addslashes($_POST['result'])."','".addslashes($_POST['season'])."');";
-        //echo $sql."<br>"; //debug
-        $result = sqlsrv_query($conn, $sql);
-        // In case of failure
-        if (!$result) {
-            die("Couldn't add the game specified.<br>");
+    <h2 style="font-size:20px;color:white">
+        Another game played? Add it here!
+    </h2>
+
+    <?php
+        include 'db_connection.php';
+        $conn = OpenCon(); // Connect to the database
+    ?>
+
+<!--    Data Insertion Form   -->
+    <form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
+        <table class="insertion_table">
+            <tr>
+                <td><label for="Home">Home Team:</label></td>
+                <td><select name="Home">
+                    <option value="">Choose home team</option>
+                    <?php
+                        // Dynamically display Teams in dropdown list
+                        $sql = "SELECT Home FROM PremierLeague";
+                        $result = sqlsrv_query($conn, $sql);
+                        while($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC))
+                        {
+                            echo "<option value=".$row['Home'].">".addslashes($row['Home'])."</option>";
+                        }
+                    ?>
+                </select></td>
+            </tr>
+            <tr>
+                <td><label for="Away">Away Team:</label></td>
+                <td><select name="Away">
+                    <option value="">Choose away team</option>
+                    <?php
+                    // Dynamically display Teams in dropdown list
+                        $sql = "SELECT Away FROM PremierLeague";
+                        $result = sqlsrv_query($conn, $sql);
+                        while($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC))
+                        {
+                            echo "<option value=".$row['Away'].">".addslashes($row['Away'])."</option>";
+                        }
+                    ?>
+                    </select></td>
+            </tr>
+            <tr>
+                <td><label for="result">Result:</label></td>
+            </tr>
+            <tr>
+                <td><input name="result" type="radio" value="H"><label for="H">H</label><br>
+                    <input name="result" type="radio" value="D"><label for="D">D</label><br>
+                    <input name="result" type="radio" value="A"><label for="A">A</label></td>
+            </tr>
+            <tr style="padding-bottom: 5px">
+                <td><label for="home_goals">Home Goals</label></td>
+                <td><input name="home_goals" type="range" step="1" min="0" max="10"></td>
+            </tr>
+            <tr style="padding-bottom: 5px">
+                <td><label for="away Goals">Away Goals</label></td>
+                <td><input name="away_goals" type="range" step="1" min="0" max="10"></td>
+            </tr>
+            <tr style="padding-bottom: 5px">
+                <td><label for="season">Season*</label></td>
+                <td><input name="season" type="text" size="24" required></td>
+            </tr>
+            <tr style="padding-bottom: 5px">
+                <td colspan="2"><textarea name="notes" Rows="5" cols="36" placeholder="Write here a special note"></textarea></td>
+            </tr>
+            <tr>
+                <td><input name="submit" type="submit" value="Add Result"></td>
+                <td><input name="reset" type="reset" value="Reset Page"></td>
+            </tr>
+        </table>
+    </form>
+
+    <?php
+        if (isset($_POST["submit"]))
+        {
+            // Get last ID, increment by one and set as new ID
+            $sql = "SELECT max(id) newID FROM PremierLeague";
+            $IDtable = sqlsrv_query($conn, $sql);
+            $row = sqlsrv_fetch_array($IDtable, SQLSRV_FETCH_ASSOC);
+            $id = $row['newID'] + 1;
+
+            // Insert data into PremierLeague table
+            $sql = "INSERT INTO PremierLeague (id, Home, Away, notes, home_goals, away_goals, result, season) VALUES
+                   ('".addslashes($id)."','".addslashes($_POST['Home'])."','".addslashes($_POST['Away'])."'
+                   ,'".addslashes($_POST['notes'])."','".addslashes($_POST['home_goals'])."','".addslashes($_POST['away_goals'])."'
+                   ,'".addslashes($_POST['result'])."','".addslashes($_POST['season'])."');";
+
+            $result = sqlsrv_query($conn, $sql);
+
+            // In case of failure
+            if (!$result) {
+                die("Couldn't add the game specified.<br>");
+            }
         }
-    }
     ?>
 </body>
 </html>
