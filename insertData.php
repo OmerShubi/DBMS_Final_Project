8890b65eba96da9bbb8ed5402046d74e8dcb6e07
OS\Shubi <omer.shubi@gmail.com>
2019-05-08 04:41:14 +0300

    General styling

--- insertData.php
+++ insertData.php
@@ -53,7 +53,7 @@
         </tr>
         <tr></tr>
         <tr>
-            <td><label for="Away Goals">Away Goals</label></td>
+            <td><label for="away Goals">Away Goals</label></td>
             <td><input name="away_goals" type="range" step="1" min="0" max="10"></td>
         </tr>
         <tr></tr>
@@ -61,8 +61,9 @@
             <td><label for="season">Season*</label></td>
             <td><input name="season" type="text" size="22" required></td>
         </tr>
+        <tr></tr>
         <tr>
-            <td colspan="2"><textarea name="notes" Rows="5" cols="40">Write here a special note</textarea></td>
+            <td colspan="2"><textarea name="notes" Rows="5" cols="40" placeholder="Write here a special note"></textarea></td>
         </tr>
         <tr>
             <td><input name="submit" type="submit" value="Add Result"></td>
