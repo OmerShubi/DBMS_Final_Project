444833a909ce5f115c6216c99e0ffbb531af43c3
scaperex <omer.shubi@gmail.com>
2019-05-18 09:24:17 +0300

    Final

--- index.php
+++ index.php
@@ -10,29 +10,29 @@
 
 <body class="main">
 
-<img src="banner.jpg" alt="Premier League Logo" style="width: 80%; border: 1px solid black">
+    <img src="banner.jpg" alt="Premier League Logo" style="width: 80%; border: 1px solid black">
 
-<div class="layer">
+    <div class="layer">
 
-    <h1>
-        <br>Premier Pro Analytics is an advanced Database & Analytics tool <br>
-    </h1>
-   <div style="background-color: rgba(255,255,255,0.7)">
-       <h2  style="color: #2e0e6b; vertical-align: center">
-           <i>for Professionals by Professionals.</i>
-       </h2>
-   </div>
-<h3>
-        <a href="displayTable.php" target="MainFrame">DISPLAY TABLE</a>
-        &nbsp;&nbsp;&nbsp;
-        <a href="addDataFile.php" target="MainFrame">ADD DATA FILE</a>
-        &nbsp;&nbsp;&nbsp;
-        <a href="insertData.php" target="MainFrame">INSERT DATA</a><br>
-    </h3>
-    <iframe name="MainFrame" src="displayTable.php"></iframe>
-    <br>
+        <h1>
+            <br>Premier Pro Analytics is an advanced Database & Analytics tool <br>
+        </h1>
+       <div style="background-color: rgba(255,255,255,0.7)">
+           <h2  style="color: #2e0e6b; vertical-align: center">
+               <i>for Professionals by Professionals.</i>
+           </h2>
+       </div>
+    <h3>
+            <a href="displayTable.php" target="MainFrame">DISPLAY TABLE</a>
+            &nbsp;&nbsp;&nbsp;
+            <a href="addDataFile.php" target="MainFrame">ADD DATA FILE</a>
+            &nbsp;&nbsp;&nbsp;
+            <a href="insertData.php" target="MainFrame">INSERT DATA</a><br>
+        </h3>
+        <iframe name="MainFrame" src="displayTable.php"></iframe>
+        <br>
 
-<footer style="color: #2e0e6b"><br>&copy; COPYRIGHT 2019 | PLOZNIK & SHUBI</footer>
-</div>
+    <footer style="color: #2e0e6b"><br>&copy; COPYRIGHT 2019 | PLOZNIK & SHUBI</footer>
+    </div>
 </body>
 </html>
