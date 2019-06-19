d626712010a0e2f9bb02ca27a2ed0faba7d8493d
scaperex <omer.shubi@gmail.com>
2019-06-19 21:19:08 +0300

    WIP

--- index.php
+++ index.php
@@ -4,30 +4,31 @@
 
 <head>
     <link rel="stylesheet" href="style.css" type="text/css">
-    <title>Premier Pro Analytics</title>
+    <title>Trek Pro Analytics</title>
     <link href="favicon.png" rel="icon" type="image/x-icon"/>
 </head>
 
 <body class="main">
 
-    <img src="banner.jpg" alt="Premier League Logo" style="width: 80%; border: 1px solid black">
+    <img src="banner.jpg" alt="Trek Pro Analytics Logo" style="width: 80%; border: 1px solid black">
 
     <div class="layer">
 
         <h1>
-            <br>Premier Pro Analytics is an advanced Database & Analytics tool <br>
+            <br>Trek Pro Analytics is an advanced Database & Analytics tool <br>
         </h1>
        <div style="background-color: rgba(255,255,255,0.7)">
            <h2  style="color: #2e0e6b; vertical-align: center">
-               <i>for Professionals by Professionals.</i>
+               <i>for Hikers by Hikers.</i>
            </h2>
        </div>
     <h3>
-            <a href="displayTable.php" target="MainFrame">DISPLAY TABLE</a>
+            <a href="displayTable.php" target="MainFrame">DISPLAY STATS</a>
             &nbsp;&nbsp;&nbsp;
-            <a href="addDataFile.php" target="MainFrame">ADD DATA FILE</a>
+            <a href="addDataFile.php" target="MainFrame">LOAD DATA</a>
             &nbsp;&nbsp;&nbsp;
-            <a href="insertData.php" target="MainFrame">INSERT DATA</a><br>
+            <a href="insertData.php" target="MainFrame">ADD TRAVELER</a>
+            <a href="storyMap.php" target="MainFrame">STORY MAP</a><br>
         </h3>
         <iframe name="MainFrame" src="displayTable.php"></iframe>
         <br>
