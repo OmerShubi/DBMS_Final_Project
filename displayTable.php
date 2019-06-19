4cfd92296a0b8ec1fd7bf68cd0a8bf67101f32cf
scaperex <omer.shubi@gmail.com>
2019-06-19 21:41:58 +0300

    WIP

--- displayTable.php
+++ displayTable.php
@@ -13,49 +13,7 @@
 
         // Creates the table. If already exists the query sends back an error, but has no effect
         // So continues program as expected.
-        $sql = "create table PremierLeague(
-                                      id int primary key,
-                                      Home varchar(50),
-                                      Away varchar(50),
-                                      notes varchar(5000),
-                                      home_goals int,
-                                      away_goals int,
-                                      result varchar(1),
-                                      season varchar(10)
-                 );";
-
-        $sql = "CREATE TABLE Trek(
-        trekName      VARCHAR(20) PRIMARY KEY,
-  length        FLOAT,
-  LAT			FLOAT,
-  LONG			FLOAT
-);
-
-CREATE TABLE TrekInCountry(
-        countryName   VARCHAR(20),
-  trekName      VARCHAR(20),
-  PRIMARY KEY (countryName, trekName),
-  FOREIGN KEY (trekName) REFERENCES Trek(trekName) ON DELETE CASCADE
-);
-
-CREATE TABLE Hiker(
-        ID             INTEGER PRIMARY KEY,
-  fullName       VARCHAR(20) NOT NULL ,
-  originCountry  VARCHAR(20) NOT NULL ,
-  Smoker         VARCHAR(3),
-  Fitness        INTEGER,
-  CHECK (Smoker='Yes' OR Smoker='No'),
-  CHECK (Fitness>=0 AND Fitness<=100)
-)
-
-CREATE TABLE HikerInTrek(
-        hikerID         INTEGER,
-  trekName        VARCHAR(20),
-  startDate       DATE,
-  PRIMARY KEY (hikerID, trekName),
-  FOREIGN KEY (hikerID) REFERENCES Hiker(ID) ON DELETE CASCADE,
-  FOREIGN KEY (trekName) REFERENCES Trek ON DELETE CASCADE
-);";
+        $sql = file_get_contents('Create_Tables.sql');
         sqlsrv_query($conn, $sql);
 
         // Displays the Table
